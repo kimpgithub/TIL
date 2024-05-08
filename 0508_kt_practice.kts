61dc3cd5e3588f45328737eb3365392b6c7deb51
kimpgithub <114399262+kimpgithub@users.noreply.github.com>
2024-05-08 16:47:20 +0900

    Update 0508_kt_practice.kts
    오타 수정

--- 0508_kt_practice.kts
+++ 0508_kt_practice.kts
@@ -1,24 +1,26 @@
 class Car(
     private val model: String,
-    val speed: Int,
+    var speed: Int,
     var gas: Int
 ) {
 
     private val plusSpeed = 10
     fun increaseSpeed(): Int {
-        return speed + plusSpeed
+        speed += plusSpeed
+        return speed
     }
 
     fun addGas(newGas: Int): Int {
         gas += newGas
         return gas
-    }   
+    }
 }
 
 val myCar = Car(
     "socar", 50, 100
 )
 
-println(myCar.speed) // Prints 50
-println(myCar.increaseSpeed()) // Prints 60
-println(myCar.addGas(20)) // Prints 120
+println(myCar.speed)
+println(myCar.increaseSpeed())
+println(myCar.addGas(20))
+println(myCar.increaseSpeed())
